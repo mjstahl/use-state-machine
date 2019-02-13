226b395b764e2036313a0e8a72a2052d89936ce2
Mark Stahl <mjstahl@gmail.com>
2019-02-13 13:19:08 -0500

    Initial pass at the implementation. No tests

---
+++ index.js
@@ -0,0 +1,19 @@
+const { Stated } = require('@mjstahl/stated')
+const { useState } = require('react')
+
+function useStateMachine (states) {
+  const machine = (states instanceof Stated)
+    ? states
+    : new Stated(states)
+  const [state, setState] = useState(machine.value)
+
+  state.onTransition((updated) => setState(updated.value))
+
+  const current = { state: machine.state, value: machine.value }
+  return [current, machine.to, machine.actions]
+}
+
+module.exports = {
+  StateMachine: Stated,
+  useStateMachine
+}
