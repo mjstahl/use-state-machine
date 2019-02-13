cd51169654b2c52bd8272a952024bfbccc0c8f25
Mark Stahl <mjstahl@gmail.com>
2019-02-13 13:44:27 -0500

    Code formatting

--- index.js
+++ index.js
@@ -2,18 +2,17 @@ const { Stated } = require('@mjstahl/stated')
 const { useState } = require('react')
 
 function useStateMachine (states) {
-  const machine = (states instanceof Stated)
-    ? states
-    : new Stated(states)
+  const machine = (states instanceof Stated) ? states : new Stated(states)
   const [state, setState] = useState(machine.value)
 
   state.onTransition((updated) => setState(updated.value))
-
-  const current = { state: machine.state, value: machine.value }
-  return [current, machine.to, machine.actions]
+  return [{
+      state: machine.state,
+      value: machine.value
+    },
+    machine.to,
+    machine.actions
+  ]
 }
 
-module.exports = {
-  StateMachine: Stated,
-  useStateMachine
-}
+module.exports = { StateMachine: Stated, useStateMachine }
